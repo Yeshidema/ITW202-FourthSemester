2d9ea403d52304a2e3f41176b99826dc81b1f155
Yeshidema <12190106.gcit@rub.edu.bt>
2021-05-10 13:16:36 +0600

    User navigation to slide

--- TODO/TODO_15/app/src/main/java/com/example/todo_15/MainActivity.java
+++ TODO/TODO_15/app/src/main/java/com/example/todo_15/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       /* androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
 
